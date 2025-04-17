5c40fbf53b8e03e4d416fe4f69a315cc29ec4461
William Menezes Damascena <williamenezes@outlook.com>
2025-04-17 09:20:16 -0300

    :recycle: refactor: Improving sidebar layout

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -1,25 +1,72 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { LayoutService } from './service/app.layout.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-menu',
-    templateUrl: './app.menu.component.html'
+    templateUrl: './app.menu.component.html',
+    styles: [
+        `
+            :host ::ng-deep .layout-menu {
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+            }
+        `,
+    ],
 })
 export class AppMenuComponent implements OnInit {
-
     model: any[] = [];
+    bottomModel: any[] = [];
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService, private router: Router) {}
 
     ngOnInit() {
         this.model = [
             {
-                label: '',
+                label: 'Dashboard',
                 items: [
-                    { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['/bmi'] }
-                ]
+                    {
+                        label: 'Home',
+                        icon: 'pi pi-fw pi-home',
+                        routerLink: ['/dashboard'],
+                    },
+                    {
+                        label: 'Minha avaliação',
+                        icon: 'pi pi-plus',
+                        routerLink: ['dashboard/avaliacao'],
+                    },
+                ],
             },
         ];
+
+        this.bottomModel = [
+            {
+                items: [
+                    {
+                        label: 'Deletar conta',
+                        icon: 'pi pi-user-minus',
+                        command: () => {
+                            //this.confirmarDelecaoConta();
+                        },
+                    },
+                    {
+                        label: 'Sair',
+                        icon: 'pi pi-sign-out',
+                        routerLink: ['auth/login'],
+                        command: () => {
+                            this.logout();
+                        },
+                    },
+                ],
+            },
+        ];
+    }
+
+    logout() {
+        this.router.navigate(['login']);
+        //this.authService.logout();
     }
 }
--- src/app/view/components/dashboard/dashboard-routing.module.ts
+++ src/app/view/components/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AssessmentComponent } from './assessment/assessment.component';
+import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'avaliacao', pathMatch: 'full' },
+    { path: '', component: DashboardComponent },
     {
         path: 'avaliacao',
         component: AssessmentComponent,
