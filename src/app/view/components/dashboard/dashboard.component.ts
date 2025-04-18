ac1a47f322b2ba25cbfa6c859a42b3ba53e4132e
William Menezes Damascena <williamenezes@outlook.com>
2025-04-18 13:28:10 -0300

    :recycle: refactor: Improving dashboard component

--- src/app/view/components/dashboard/dashboard.component.ts
+++ src/app/view/components/dashboard/dashboard.component.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
+import { CardModule } from 'primeng/card';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    selector: 'app-dashboard',
+    standalone: true,
+    imports: [PanelModule, CardModule, ChartModule],
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+    user = {
+        height: '1,75',
+        weight: '72',
+        bmi: '23.51',
+    };
 
+    data: any;
+    options: any;
+
+    ngOnInit() {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue(
+            '--text-color-secondary'
+        );
+        const surfaceBorder =
+            documentStyle.getPropertyValue('--surface-border');
+
+        this.data = {
+            labels: [
+                'Jan',
+                'Fev',
+                'Mar',
+                'Abr',
+                'Mai',
+            ],
+            datasets: [
+                {
+                    label: 'Peso',
+                    data: [72, 70, 75, 81, 73],
+                    fill: true,
+                    borderColor: documentStyle.getPropertyValue('--blue-500'),
+                    tension: 0.4,
+                    backgroundColor: 'rgba(59, 131, 246, 0.185)',
+                },
+            ],
+        };
+
+        this.options = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.6,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor,
+                        usePointStyle: true,
+                        pointStyle: 'circle',
+                    },
+                    position: 'bottom',
+                },
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false,
+                    },
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary,
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false,
+                    },
+                },
+            },
+        };
+    }
 }
