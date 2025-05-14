93a76223a4a7ca93b0b9f49042b39d2d4e34b8b9
William Menezes Damascena <williamenezes@outlook.com>
2025-05-14 19:56:36 -0300

    :sparkles: feat: Implementing auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,52 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { AppLayoutComponent } from "./layout/app.layout.component";
+import { AppLayoutComponent } from './layout/app.layout.component';
+import { authGuard } from './guard/auth.guard';
 
 @NgModule({
     imports: [
-        RouterModule.forRoot([
+        RouterModule.forRoot(
+            [
+                { path: '', redirectTo: 'login', pathMatch: 'full' },
 
-            { path: '', redirectTo: 'login', pathMatch: 'full' },
+                {
+                    path: 'login',
+                    loadChildren: () =>
+                        import(
+                            './view/components/auth/login/login.module'
+                        ).then((m) => m.LoginModule),
+                },
+                {
+                    path: 'signup',
+                    loadChildren: () =>
+                        import(
+                            './view/components/auth/signup/signup.module'
+                        ).then((m) => m.SignupModule),
+                },
 
-            { path: 'login', loadChildren: () => import('./view/components/auth/login/login.module').then(m => m.LoginModule) },
-            { path: 'signup', loadChildren: () => import('./view/components/auth/signup/signup.module').then(m => m.SignupModule) },
-
-
+                {
+                    path: '',
+                    component: AppLayoutComponent,
+                    children: [
+                        /* { path: 'bmi', loadChildren: () => import('./view/components/bmi/bmi.module').then(m => m.BmiModule) }, */
+                        {
+                            path: 'dashboard',
+                            loadChildren: () =>
+                                import(
+                                    './view/components/dashboard/dashboard.module'
+                                ).then((m) => m.DashboardModule),
+                        },
+                    ],
+                    canActivate: [authGuard],
+                },
+            ],
             {
-                path: '', component: AppLayoutComponent,
-                children: [
-                    /* { path: 'bmi', loadChildren: () => import('./view/components/bmi/bmi.module').then(m => m.BmiModule) }, */
-                    { path: 'dashboard', loadChildren: () => import('./view/components/dashboard/dashboard.module').then(m => m.DashboardModule)}
-                ]
-            },
-        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+                scrollPositionRestoration: 'enabled',
+                anchorScrolling: 'enabled',
+                onSameUrlNavigation: 'reload',
+            }
+        ),
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
